301ef155bd02a802409bc75ba2a770f8f12e8d33
q454067820 <454067820@qq.com>
2016-09-28 21:38:17 +0800

    增加第二章习题

---
+++ C_Program_2_exsersice.h
@@ -0,0 +1,17 @@
+//
+// Created by 张嘉霖 on 16/9/27.
+//
+
+#ifndef UNTITLED_C_PROGRAM_2_EXSERSICE_H
+#define UNTITLED_C_PROGRAM_2_EXSERSICE_H
+
+
+int g_atoi (char s[]);
+int g_lower(char c);
+int g_upper(char c);
+void g_squeeze(char s[],char c);
+void g_strcat(char s[], char c[]);
+void g_squeeze3(char s[],char c[]);
+int any (char s[], char c[]);
+
+#endif //UNTITLED_C_PROGRAM_2_EXSERSICE_H
---
+++ main.c
@@ -0,0 +1,14 @@
+//
+// Created by 张嘉霖 on 16/9/6.
+//
+#include <stdio.h>      // 包含标准库的信息
+#include <stdlib.h>      // 包含标准库的信息
+#include "C_Program_2_exsersice.h"
+
+
+int main(){
+    char c = 'A';
+    int n = g_upper(c);
+    printf("Upper is %d", n);
+}
+
